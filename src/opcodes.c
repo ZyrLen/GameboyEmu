8796c78c866daae9d8a7f5d1d2fe63cf9621a6b9
ZyrLen <gavin.mercurio@gmail.com>
2026-06-07 00:26:54 -0500

    Made bash script to remove tedious-ness of LD instructions

--- src/opcodes.c
+++ src/opcodes.c
@@ -5,8 +5,17 @@
 OpcodeEntry opcodeTable[256];
 
 // mcycles is machine cycles. 1 mycycle = 4 timer cycles
-// Note: machine cycles listed below have some errors for now, will come back to
-//       it after more of the opcodes are done
+// Note: machine cycles listed below have some errors for now, will come back to it after more of the opcodes are done
+
+/* Might do something like this if memory usage is too high later*/
+
+// char *regStr[] = {"B", "C", "D", "E", "H", "L", "(HL)", "A"};
+// u8 *regs[] = {&gb->B, &gb->C, &gb->D, 
+//							&gb->E, &gb->H, &gb->L, &gb->HL, &gb->A};
+
+// if (0x40 <= opcode && opcode < 0x80) {
+// 	return (OpcodeEntry){.handler = LD_R_R, .R1 = regs[]}
+// }
 
 void initOpcodeTable(Gameboy *gb) {
   opcodeTable[0x0] =
@@ -116,9 +125,7 @@ void initOpcodeTable(Gameboy *gb) {
     .handler = LD_R_u8, .arg = &gb->L, .mcycles = 2, .mnemonic = "LD L,u8"};
 
   opcodeTable[0x31] = (OpcodeEntry){.handler = LD_SP_u16,
-    .arg = &gb->sp,
-    .mcycles = 3,
-    .mnemonic = "LD SP,u16"};
+    .arg = &gb->sp, .mcycles = 3, .mnemonic = "LD SP,u16"};
 
   opcodeTable[0x32] = (OpcodeEntry){
     .handler = LD_HLminus_A, .mcycles = 2, .mnemonic = "LD (HL-),A"};
@@ -129,387 +136,203 @@ void initOpcodeTable(Gameboy *gb) {
   opcodeTable[0x3E] = (OpcodeEntry){
     .handler = LD_R_u8, .arg = &gb->A, .mcycles = 2, .mnemonic = "LD A,u8"};
 
-  opcodeTable[0x40] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD B,B"};
+	opcodeTable[0x40] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->B,
+		.mcycles = 1, .mnemonic = "LD B,B"};
 
-  opcodeTable[0x41] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD B,C"};
+	opcodeTable[0x41] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->C,
+		.mcycles = 1, .mnemonic = "LD B,C"};
 
-  opcodeTable[0x42] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD B,D"};
+	opcodeTable[0x42] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->D,
+		.mcycles = 1, .mnemonic = "LD B,D"};
 
-  opcodeTable[0x43] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD B,E"};
+	opcodeTable[0x43] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->E,
+		.mcycles = 1, .mnemonic = "LD B,E"};
 
-  opcodeTable[0x44] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD B,H"};
+	opcodeTable[0x44] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->H,
+		.mcycles = 1, .mnemonic = "LD B,H"};
 
-  opcodeTable[0x45] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD B,L"};
+	opcodeTable[0x45] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->L,
+		.mcycles = 1, .mnemonic = "LD B,L"};
 
-  opcodeTable[0x46] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->B,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD B,(HL)"};
+	opcodeTable[0x46] = (OpcodeEntry){.handler = LD_R_Addr,
+		.arg = &gb->B, .address = gb->HL,
+		.mcycles = 1, .mnemonic = "LD B,(HL)"};
 
-  opcodeTable[0x47] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->B,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD B,A"};
+	opcodeTable[0x47] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->B, .R2 = &gb->A,
+		.mcycles = 1, .mnemonic = "LD B,A"};
 
-  opcodeTable[0x48] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD C,B"};
+	opcodeTable[0x48] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->B,
+		.mcycles = 1, .mnemonic = "LD C,B"};
 
-  opcodeTable[0x49] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD C,C"};
+	opcodeTable[0x49] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->C,
+		.mcycles = 1, .mnemonic = "LD C,C"};
 
-  opcodeTable[0x4A] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD C,D"};
-  opcodeTable[0x4B] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD C,E"};
+	opcodeTable[0x4A] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->D,
+		.mcycles = 1, .mnemonic = "LD C,D"};
 
-  opcodeTable[0x4C] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD C,H"};
+	opcodeTable[0x4B] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->E,
+		.mcycles = 1, .mnemonic = "LD C,E"};
 
-  opcodeTable[0x4D] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD C,L"};
+	opcodeTable[0x4C] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->H,
+		.mcycles = 1, .mnemonic = "LD C,H"};
 
-  opcodeTable[0x4E] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->C,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD C,(HL)"};
+	opcodeTable[0x4D] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->L,
+		.mcycles = 1, .mnemonic = "LD C,L"};
 
-  opcodeTable[0x4F] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->C,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD C,A"};
+	opcodeTable[0x4E] = (OpcodeEntry){.handler = LD_R_Addr,
+		.arg = &gb->C, .address = gb->HL,
+		.mcycles = 1, .mnemonic = "LD C,(HL)"};
 
-  opcodeTable[0x50] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD D,B"};
+	opcodeTable[0x4F] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->C, .R2 = &gb->A,
+		.mcycles = 1, .mnemonic = "LD C,A"};
 
-  opcodeTable[0x51] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD D,C"};
+	opcodeTable[0x50] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->B,
+		.mcycles = 1, .mnemonic = "LD D,B"};
 
-  opcodeTable[0x52] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD D,D"};
+	opcodeTable[0x51] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->C,
+		.mcycles = 1, .mnemonic = "LD D,C"};
 
-  opcodeTable[0x53] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD D,E"};
+	opcodeTable[0x52] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->D,
+		.mcycles = 1, .mnemonic = "LD D,D"};
 
-  opcodeTable[0x54] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD D,H"};
+	opcodeTable[0x53] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->E,
+		.mcycles = 1, .mnemonic = "LD D,E"};
 
-  opcodeTable[0x55] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD D,L"};
+	opcodeTable[0x54] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->H,
+		.mcycles = 1, .mnemonic = "LD D,H"};
 
-  opcodeTable[0x56] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->D,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD D,(HL)"};
+	opcodeTable[0x55] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->L,
+		.mcycles = 1, .mnemonic = "LD D,L"};
 
-  opcodeTable[0x57] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->D,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD D,A"};
+	opcodeTable[0x56] = (OpcodeEntry){.handler = LD_R_Addr,
+		.arg = &gb->D, .address = gb->HL,
+		.mcycles = 1, .mnemonic = "LD D,(HL)"};
 
-  opcodeTable[0x58] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD E,B"};
+	opcodeTable[0x57] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->D, .R2 = &gb->A,
+		.mcycles = 1, .mnemonic = "LD D,A"};
 
-  opcodeTable[0x59] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD E,C"};
+	opcodeTable[0x58] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->B,
+		.mcycles = 1, .mnemonic = "LD E,B"};
 
-  opcodeTable[0x5A] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD E,D"};
+	opcodeTable[0x59] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->C,
+		.mcycles = 1, .mnemonic = "LD E,C"};
 
-  opcodeTable[0x5B] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD E,E"};
+	opcodeTable[0x5A] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->D,
+		.mcycles = 1, .mnemonic = "LD E,D"};
 
-  opcodeTable[0x5C] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD E,H"};
+	opcodeTable[0x5B] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->E,
+		.mcycles = 1, .mnemonic = "LD E,E"};
 
-  opcodeTable[0x5D] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD E,L"};
+	opcodeTable[0x5C] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->H,
+		.mcycles = 1, .mnemonic = "LD E,H"};
 
-  opcodeTable[0x5E] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->E,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD E,(HL)"};
+	opcodeTable[0x5D] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->L,
+		.mcycles = 1, .mnemonic = "LD E,L"};
 
-  opcodeTable[0x5F] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->E,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD E,A"};
+	opcodeTable[0x5E] = (OpcodeEntry){.handler = LD_R_Addr,
+		.arg = &gb->E, .address = gb->HL,
+		.mcycles = 1, .mnemonic = "LD E,(HL)"};
 
-  opcodeTable[0x60] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD H,B"};
+	opcodeTable[0x5F] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->E, .R2 = &gb->A,
+		.mcycles = 1, .mnemonic = "LD E,A"};
 
-  opcodeTable[0x61] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD H,C"};
+	opcodeTable[0x60] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->B,
+		.mcycles = 1, .mnemonic = "LD H,B"};
 
-  opcodeTable[0x62] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD H,D"};
+	opcodeTable[0x61] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->C,
+		.mcycles = 1, .mnemonic = "LD H,C"};
 
-  opcodeTable[0x63] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD H,E"};
+	opcodeTable[0x62] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->D,
+		.mcycles = 1, .mnemonic = "LD H,D"};
 
-  opcodeTable[0x64] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD H,H"};
+	opcodeTable[0x63] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->E,
+		.mcycles = 1, .mnemonic = "LD H,E"};
 
-  opcodeTable[0x65] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD H,L"};
+	opcodeTable[0x64] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->H,
+		.mcycles = 1, .mnemonic = "LD H,H"};
 
-  opcodeTable[0x66] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->H,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD H,(HL)"};
+	opcodeTable[0x65] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->L,
+		.mcycles = 1, .mnemonic = "LD H,L"};
 
-  opcodeTable[0x67] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->H,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD H,A"};
+	opcodeTable[0x66] = (OpcodeEntry){.handler = LD_R_Addr,
+		.arg = &gb->H, .address = gb->HL,
+		.mcycles = 1, .mnemonic = "LD H,(HL)"};
 
-  opcodeTable[0x68] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD L,B"};
+	opcodeTable[0x67] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->H, .R2 = &gb->A,
+		.mcycles = 1, .mnemonic = "LD H,A"};
 
-  opcodeTable[0x69] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD L,C"};
+	opcodeTable[0x68] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->B,
+		.mcycles = 1, .mnemonic = "LD L,B"};
 
-  opcodeTable[0x6A] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD L,D"};
+	opcodeTable[0x69] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->C,
+		.mcycles = 1, .mnemonic = "LD L,C"};
 
-  opcodeTable[0x6B] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD L,E"};
+	opcodeTable[0x6A] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->D,
+		.mcycles = 1, .mnemonic = "LD L,D"};
 
-  opcodeTable[0x6C] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD L,H"};
+	opcodeTable[0x6B] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->E,
+		.mcycles = 1, .mnemonic = "LD L,E"};
 
-  opcodeTable[0x6D] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD L,L"};
+	opcodeTable[0x6C] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->H,
+		.mcycles = 1, .mnemonic = "LD L,H"};
 
-  opcodeTable[0x6E] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->L,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD L,(HL)"};
+	opcodeTable[0x6D] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->L,
+		.mcycles = 1, .mnemonic = "LD L,L"};
 
-  opcodeTable[0x6F] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->L,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD L,A"};
+	opcodeTable[0x6E] = (OpcodeEntry){.handler = LD_R_Addr,
+		.arg = &gb->L, .address = gb->HL,
+		.mcycles = 1, .mnemonic = "LD L,(HL)"};
 
-  opcodeTable[0x70] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->B,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),B"};
+	opcodeTable[0x6F] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->L, .R2 = &gb->A,
+		.mcycles = 1, .mnemonic = "LD L,A"};
 
-  opcodeTable[0x71] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->C,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),C"};
+  opcodeTable[0x70] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->B, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),B"};
 
-  opcodeTable[0x72] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->D,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),D"};
+  opcodeTable[0x71] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->C, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),C"};
 
-  opcodeTable[0x73] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->E,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),E"};
+  opcodeTable[0x72] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->D, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),D"};
 
-  opcodeTable[0x74] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->H,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),H"};
+  opcodeTable[0x73] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->E, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),E"};
 
-  opcodeTable[0x75] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->L,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),L"};
+  opcodeTable[0x74] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->H, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),H"};
 
-  /// HALT ///
+  opcodeTable[0x75] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->L, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),L"};
 
   opcodeTable[0x76] =
     (OpcodeEntry){.handler = HALT, .mcycles = 1, .mnemonic = "HALT"};
 
-  opcodeTable[0x77] = (OpcodeEntry){.handler = LD_Addr_R,
-    .arg = &gb->A,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD (HL),A"};
+  opcodeTable[0x77] = (OpcodeEntry){.handler = LD_Addr_R, .arg = &gb->A, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD (HL),A"};
 
-  opcodeTable[0x78] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->B,
-    .mcycles = 1,
-    .mnemonic = "LD A,B"};
+  opcodeTable[0x78] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->B,
+    .mcycles = 1, .mnemonic = "LD A,B"};
 
-  opcodeTable[0x79] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->C,
-    .mcycles = 1,
-    .mnemonic = "LD A,C"};
+  opcodeTable[0x79] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->C,
+    .mcycles = 1, .mnemonic = "LD A,C"};
 
-  opcodeTable[0x7A] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->D,
-    .mcycles = 1,
-    .mnemonic = "LD A,D"};
+  opcodeTable[0x7A] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->D,
+    .mcycles = 1, .mnemonic = "LD A,D"};
 
-  opcodeTable[0x7B] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->E,
-    .mcycles = 1,
-    .mnemonic = "LD A,E"};
+  opcodeTable[0x7B] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->E,
+    .mcycles = 1, .mnemonic = "LD A,E"};
 
-  opcodeTable[0x7C] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->H,
-    .mcycles = 1,
-    .mnemonic = "LD A,H"};
+  opcodeTable[0x7C] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->H,
+    .mcycles = 1, .mnemonic = "LD A,H"};
 
-  opcodeTable[0x7D] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->L,
-    .mcycles = 1,
-    .mnemonic = "LD A,L"};
+  opcodeTable[0x7D] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->L,
+    .mcycles = 1, .mnemonic = "LD A,L"};
 
-  opcodeTable[0x7E] = (OpcodeEntry){.handler = LD_R_Addr,
-    .arg = &gb->A,
-    .address = gb->HL,
-    .mcycles = 2,
-    .mnemonic = "LD A,(HL)"};
+  opcodeTable[0x7E] = (OpcodeEntry){.handler = LD_R_Addr, .arg = &gb->A, .address = gb->HL,
+    .mcycles = 2, .mnemonic = "LD A,(HL)"};
 
-  opcodeTable[0x7F] = (OpcodeEntry){.handler = LD_R_R,
-    .R1 = &gb->A,
-    .R2 = &gb->A,
-    .mcycles = 1,
-    .mnemonic = "LD A,A"};
+  opcodeTable[0x7F] = (OpcodeEntry){.handler = LD_R_R, .R1 = &gb->A, .R2 = &gb->A,
+    .mcycles = 1, .mnemonic = "LD A,A"};
 
   opcodeTable[0x90] = (OpcodeEntry){
     .handler = SUB_R, .arg = &gb->B, .mcycles = 1, .mnemonic = "SUB A,B"};
